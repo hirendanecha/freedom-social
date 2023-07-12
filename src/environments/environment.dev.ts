7b3d840e37ed24b325b038a01b5ab76888a20054
Swapnil Opash <sra.opash@gmail.com>
2023-07-12 14:51:15 +0530

    env changes

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   hmr: false,
-  serverUrl: 'http://localhost:5000/api/v1/',
-  serverUrl2: 'https://lamarslist.com:8443/api/v1/',
+  serverUrl2: 'http://localhost:5000/api/v1/',
+  serverUrl: 'https://lamarslist.com:5000/api/v1/',
 };
