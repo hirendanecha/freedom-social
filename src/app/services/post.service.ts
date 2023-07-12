54b6a301a7023f27586683849fd3730cbe584896
Swapnil Opash <sra.opash@gmail.com>
2023-07-12 18:28:29 +0530

    api path changes

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -14,6 +14,6 @@ export class PostService {
   constructor(private http: HttpClient) {}
 
   getPosts() {
-    return this.http.get(`${this.baseUrl}/posts/`);
+    return this.http.get(`${this.baseUrl}posts/`);
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   hmr: false,
-  serverUrl: 'http://localhost:8080/api/v1',
-  serverUrl2: 'https://lamarslist.com:8080/api/v1',
+  serverUrl: 'http://localhost:8080/api/v1/',
+  serverUrl2: 'https://lamarslist.com:8080/api/v1/',
 };
