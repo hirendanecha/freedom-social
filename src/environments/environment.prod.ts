e17fd7d008b470a1ef37ba6ae2d68529fa0b64f2
Swapnil Opash <sra.opash@gmail.com>
2023-07-14 12:41:27 +0530

    env changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   hmr: false,
-  serverUrl: 'http://localhost:8080/api/v1/',
+  serverUrl: 'https://freedom-api.opash.in/api/v1/',
   serverUrl2: 'https://lamarslist.com:8080/api/v1/',
 };
