b7cd192337acc544952b7985941f8a6dcd1ebc6c
vatsal <vso.opash@gmail.com>
2023-09-27 10:58:37 +0530

    make a onblur close img and fix img errors

--- src/app/@shared/components/img-preview/img-preview.component.ts
+++ src/app/@shared/components/img-preview/img-preview.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-img-preview',
@@ -11,4 +11,20 @@ export class ImgPreviewComponent {
   @Input('classes') classes: string = 'w-40-px h-40-px';
 
   previewSrc: string = '';
+
+  constructor(private renderer: Renderer2, private el: ElementRef) { }
+
+  openImagePreview(src: string) {
+    this.previewSrc = src;
+    this.renderer.setStyle(this.el.nativeElement.ownerDocument.body, 'overflow', 'hidden');
+  }
+
+  stopPropagation(event: Event) {
+    event.stopPropagation();
+  }
+
+  closeImagePreview() {
+    this.previewSrc = '';
+    this.renderer.removeStyle(this.el.nativeElement.ownerDocument.body, 'overflow');
+  }
 }
