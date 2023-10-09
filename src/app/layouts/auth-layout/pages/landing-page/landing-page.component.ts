82e73c1b4b63de6e0d8fd633c82c3ff6e32df8d7
vatsal <vso.opash@gmail.com>
2023-10-09 10:15:00 +0530

    Fix mobile Sidebar

--- src/app/layouts/auth-layout/pages/landing-page/landing-page.component.ts
+++ src/app/layouts/auth-layout/pages/landing-page/landing-page.component.ts
@@ -1,28 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.scss']
+  styleUrls: ['./landing-page.component.scss'],
 })
 export class LandingPageComponent {
-  mobileMenuToggle: boolean = false
+  mobileMenuToggle: boolean = false;
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private renderer: Renderer2,
+    private el: ElementRef
+   ) {}
 
   openLoginPage(): void {
-    this.mobileMenuToggle = false;
+    this.closeMenu();
     this.router.navigate(['/login']);
   }
   openSignPage(): void {
-    this.mobileMenuToggle = false;
+    this.closeMenu();
     this.router.navigate(['/register']);
   }
-  mobileMenu():void{
-  this.mobileMenuToggle = !this.mobileMenuToggle;
+  mobileMenu(): void {
+    this.mobileMenuToggle = !this.mobileMenuToggle;
+    this.renderer.setStyle(this.el.nativeElement.ownerDocument.body, 'overflow', 'hidden');
   }
-  closeMenu(){
+  closeMenu() {
     this.mobileMenuToggle = false;
+    this.renderer.removeStyle(this.el.nativeElement.ownerDocument.body, 'overflow');
   }
 }
