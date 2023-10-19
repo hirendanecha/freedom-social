545813d45ac18028657c2dbef2ef8b54a19342a8
Swapnil Opash <sra.opash@gmail.com>
2023-10-19 16:32:50 +0530

    change ts config

--- src/main.server.ts
+++ src/main.server.ts
@@ -6,7 +6,6 @@
  * available, such as `@angular/elements`.
  */
 import '@angular/platform-server/init';
-
 /***************************************************************************************************
  * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
  */
